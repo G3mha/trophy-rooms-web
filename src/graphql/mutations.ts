331a10a63a44fb68edeb69bd91044cff2e121644
Enricco Gemha <enriccogemha@gmail.com>
2025-12-25 22:17:12 -0300

    Add GraphQL mutations for games and achievements

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,106 @@
+import { gql } from "@apollo/client";
+import { GAME_FRAGMENT, ACHIEVEMENT_FRAGMENT } from "./queries";
+
+// Game mutations
+export const CREATE_GAME = gql`
+  mutation CreateGame($input: CreateGameInput!) {
+    createGame(input: $input) {
+      success
+      game {
+        ...GameFields
+      }
+      error {
+        code
+        message
+        field
+      }
+    }
+  }
+  ${GAME_FRAGMENT}
+`;
+
+export const UPDATE_GAME = gql`
+  mutation UpdateGame($id: ID!, $input: UpdateGameInput!) {
+    updateGame(id: $id, input: $input) {
+      success
+      game {
+        ...GameFields
+      }
+      error {
+        code
+        message
+        field
+      }
+    }
+  }
+  ${GAME_FRAGMENT}
+`;
+
+export const DELETE_GAME = gql`
+  mutation DeleteGame($id: ID!) {
+    deleteGame(id: $id) {
+      success
+      deletedId
+      error {
+        code
+        message
+        field
+      }
+    }
+  }
+`;
+
+// Achievement mutations
+export const CREATE_ACHIEVEMENT = gql`
+  mutation CreateAchievement($input: CreateAchievementInput!) {
+    createAchievement(input: $input) {
+      success
+      achievement {
+        ...AchievementFields
+      }
+      error {
+        code
+        message
+        field
+      }
+    }
+  }
+  ${ACHIEVEMENT_FRAGMENT}
+`;
+
+// User achievement mutations
+export const MARK_ACHIEVEMENT_COMPLETE = gql`
+  mutation MarkAchievementComplete($achievementId: ID!) {
+    markAchievementComplete(achievementId: $achievementId) {
+      success
+      userAchievement {
+        id
+        createdAt
+        achievement {
+          id
+          title
+          isCompleted
+          userCount
+        }
+      }
+      error {
+        code
+        message
+        field
+      }
+    }
+  }
+`;
+
+export const UNMARK_ACHIEVEMENT_COMPLETE = gql`
+  mutation UnmarkAchievementComplete($achievementId: ID!) {
+    unmarkAchievementComplete(achievementId: $achievementId) {
+      success
+      error {
+        code
+        message
+        field
+      }
+    }
+  }
+`;
