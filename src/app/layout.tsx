f7c061e8c56366a37834223e633abf54588d001f
Enricco Gemha <enriccogemha@gmail.com>
2026-02-09 09:32:06 +0100

    fix(clerk): force local auth routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider signInUrl="/sign-in" signUpUrl="/sign-up">
       <html lang="en">
         <body>
           <ApolloWrapper>
