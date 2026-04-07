60eef9ca62cff1b87bf7b3a476c9d9e963c58fbe
Enricco Gemha <enriccogemha@gmail.com>
2026-04-06 21:21:53 -0300

    chore(admin): export GameEditModal and GameCloneModal

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -3,3 +3,5 @@ export { AdminFeedback } from "./admin-feedback";
 export { CoverPreview } from "./cover-preview";
 export { GameSearchPicker } from "./game-search-picker";
 export type { SearchableGame } from "./game-search-picker";
+export { GameEditModal } from "./GameEditModal";
+export { GameCloneModal } from "./GameCloneModal";
